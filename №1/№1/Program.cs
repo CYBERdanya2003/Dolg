b690a0cea74c4ca73d6466304ad658de8bb5931a
CYBERdanya2003 <91051801+CYBERdanya2003@users.noreply.github.com>
2022-10-13 17:14:02 +0500

    Добавьте файлы проекта.

---
+++ №1/№1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Лунная ночь");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(@"Будет луна.
+Есть уже
+немножко.
+А вот и полная повисла в воздухе.
+Это Бог, должно быть,
+дивной
+серебряной ложкой
+роется в звёзд ухе.");
+        Console.ReadKey();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ReadKey();
+    }
+}
---
+++ №8/№8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int Res = 0;
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= n; i++)
+        {
+            Res = Res + (n + i) * (n + i);
+        }
+        Console.WriteLine(Res);
+        Console.ReadKey();
+    }
+
+}
